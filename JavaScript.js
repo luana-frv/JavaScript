fca1363eda7284386ed906144da5a6a9cc26692e
luana-frv <115549685+luana-frv@users.noreply.github.com>
2022-12-13 14:36:29 +0000

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,66 @@
+let person = "Luana";
+console.log(person)
+let age = 18;
+console.log(age*7)
+
+
+
+let friends = ["Beatriz", "Diana", "Mara"];
+console.log(friends);
+
+
+
+let Beatriz = 
+{
+name: "Beatriz",
+age: 20,
+isStudent: true
+};
+console.log(Beatriz.name);
+
+
+let Diana = 
+{
+name: "Diana",
+age: 18,
+isStudent: true
+};
+console.log(Diana.name);
+
+let Mara = 
+{
+name: "Mara",
+age: 18,
+isStudent: true
+};
+console.log(Mara.name);
+
+
+
+let friends1 =
+[
+    {
+        name: "Beatriz",
+        age: 20,
+        isStudent: true
+    },
+
+    {
+        name: "Diana",
+        age: 18,
+        isStudent: true
+    },
+
+    {
+        name: "Beatriz",
+        age: 20,
+        isStudent: true
+    }
+];
+console.log(friends1[0].name);
+
+
+
+
+
+
